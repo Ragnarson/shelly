0b1277c4ed1c72a944e2987ac38960d94a10a1ac
Mariusz Pietrzyk <wijet@wijet.pl>
2011-10-03 15:24:11 +0200

    Added setting up fake ~/.shelly dir for specs

--- spec/shelly/base_spec.rb
+++ spec/shelly/base_spec.rb
@@ -1,6 +1,11 @@
 require "spec_helper"
 
 describe Shelly::Base do
+  before do
+    config_dir = File.expand_path("~/.shelly")
+    FileUtils.mkdir_p(config_dir)
+  end
+
   describe "#current_user" do
     it "should return user with loaded credentials" do
       File.open(File.join("~/.shelly/credentials"), "w") { |f| f << "superman@example.com\nthe-kal-el" }
