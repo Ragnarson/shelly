46f6008b249fc4bd961b2b0b08935e6303110556
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2011-12-30 13:40:07 +0100

    Don't run hooks when no args are given

--- lib/thor/thor.rb
+++ lib/thor/thor.rb
@@ -6,7 +6,7 @@ class Thor
     end
 
     def send(*args)
-      if args.first == :dispatch
+      if args.first == :dispatch && !args[2].empty?
         running_task = args[2].first
         @hook.each do |method, options|
           if options[:only].include?(running_task.to_sym)
