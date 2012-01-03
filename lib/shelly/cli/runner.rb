e8b3d9a0326c1020dfc8657ac1ee97177087efd6
Mariusz Pietrzyk <wijet@wijet.pl>
2012-01-03 08:07:36 +0100

    Revert "Catch all exceptions in Runner#start, not only StandardError"
    This reverts commit 3b12c8aacc48622d405d45c21cdd33023106489c.

--- lib/shelly/cli/runner.rb
+++ lib/shelly/cli/runner.rb
@@ -17,7 +17,7 @@ module Shelly
 
       def start
         Shelly::CLI::Main.start(args)
-      rescue Exception => e
+      rescue => e
         raise e if debug?
         say_error "Unknown error, to see debug information run command with --debug"
       end
--- spec/shelly/cli/runner_spec.rb
+++ spec/shelly/cli/runner_spec.rb
@@ -52,17 +52,8 @@ describe Shelly::CLI::Runner do
     end
 
     context "without --debug option (normal mode)" do
-      it "should caught exception and display generic error message" do
+      it "should rescue exception and display generic error message" do
         Shelly::CLI::Main.stub(:start).and_raise(RuntimeError.new)
-        runner = Shelly::CLI::Runner.new(%w(version))
-        $stdout.should_receive(:puts).with("Unknown error, to see debug information run command with --debug")
-        lambda {
-          runner.start
-        }.should raise_error(SystemExit)
-      end
-
-      it "should caught exception thrown by API Client" do
-        Shelly::CLI::Main.stub(:start).and_raise(Shelly::Client::APIError.new(401))
         runner = Shelly::CLI::Runner.new(%w(version))
         $stdout.should_receive(:puts).with("Unknown error, to see debug information run command with --debug")
         lambda {
