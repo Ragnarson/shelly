09d2e313c004dfad62a9321b35316ac5ce47f263
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2012-04-11 11:02:51 +0200

    Remove Hash#deep_symbolize_keys monkeypatch
    It was not used anywhere, and was raising errors when shelly was
    included in bundle when generating app with appscrolls.

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -6,12 +6,4 @@ class Hash
     end
     new_hash
   end
-
-  def deep_symbolize_keys
-    new_hash = {}
-    self.each do |key, value|
-      new_hash.merge!(key.to_sym => (value.is_a?(Hash) ? value.deep_symbolize_keys : value))
-    end
-    new_hash
-  end
 end
