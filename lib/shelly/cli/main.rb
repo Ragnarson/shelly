ee25552b866ed5c38ccd4ca28f8cef155bc5b261
Mariusz Pietrzyk <wijet@wijet.pl>
2012-07-10 17:27:49 +0200

    Decreased logs refreshing time to 0.5s

--- lib/shelly/cli/main.rb
+++ lib/shelly/cli/main.rb
@@ -279,9 +279,10 @@ We have been notified about it. We will be adding new resources shortly}
 
         if options[:tail]
           loop do
-            logs = app.application_logs(:from => logs['range']['last'], :source => options[:source])
+            logs = app.application_logs(:from => logs['range']['last'],
+              :source => options[:source])
             print_logs(logs)
-            sleep 1
+            sleep 0.5
           end
         end
       rescue Client::APIException => e
